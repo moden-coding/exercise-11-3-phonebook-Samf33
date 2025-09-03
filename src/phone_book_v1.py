44f1619ac34cb6dade755eab7ccc101a7232d4d8
Samf33 <144719150+Samf33@users.noreply.github.com>
2025-09-03 13:37:41 -0400

    dwfocdewopwdes

--- src/phone_book_v1.py
+++ src/phone_book_v1.py
@@ -1,15 +1,20 @@
 # Write your solution here
 def search(phonebook):
-    pass
-
+    searchName = input("name: ")
+    if searchName in phonebook:
+        print(phonebook[searchName])
+    else:
+        print("no number")
 def add(phonebook):
-    pass
+    addName = input("name: ")
+    addNum = (input("number: "))
+    phonebook[addName] = addNum
+    print("ok!")
 
 def main():
     phonebook = {}
     while True:
         command = int(input("command (1 search, 2 add, 3 quit): "))
-
         if command == 1:
             search(phonebook)
         if command == 2:
